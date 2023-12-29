0e24905f780c310bc229420814725d613e8ca483
sdsanghai2232 <15110621040@atmiyauni.edu.in>
2023-12-29 19:12:25 +0530

    login page UI

---
+++ app/src/main/java/com/shd/SplashScreen.java
@@ -0,0 +1,26 @@
+package com.shd;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.shd.login.LogIn;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), LogIn.class));
+                finish();
+            }
+        },300);
+    }
+}
---
+++ app/src/main/java/com/shd/login/LogIn.java
@@ -0,0 +1,24 @@
+package com.shd.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.EditText;
+
+import com.shd.R;
+
+public class LogIn extends AppCompatActivity {
+
+    private EditText emailEditText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in);
+
+    }
+}
